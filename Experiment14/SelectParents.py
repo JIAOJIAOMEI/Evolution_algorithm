9d3d166886082db55cd305b7c9c7102a59ffb04c
JIAOJIAOMEI <90816748+JIAOJIAOMEI@users.noreply.github.com>
2023-01-02 02:36:11 +0100

    update

--- Experiment14/SelectParents.py
+++ Experiment14/SelectParents.py
@@ -2,6 +2,7 @@
 # Profession: Artificial Intelligence
 # Time and date: 1/2/23 01:49
 import random
+from typing import List, Any
 
 
 # Selection methond: Best and worst
@@ -14,7 +15,7 @@ def best_and_worst(individuals):
     eligible_individuals = [individuals[0]]
     eligible_partners = [individuals[:-1]]
     non_eligible_individuals = [individuals[-1]]
-    return eligible_individuals, eligible_partners,non_eligible_individuals
+    return eligible_individuals, eligible_partners, non_eligible_individuals
 
 
 # Selection methond: sorted
@@ -31,3 +32,54 @@ def sorted_selection(individuals, gg):
     non_eligible_individuals = [individuals[len(individuals) - size:]]
     return eligible_individuals, eligible_partners, non_eligible_individuals
 
+
+# Selection methond: rulette wheel
+# use individual.fitness to calculate the probability of being selected
+# the probability of being selected is the fitness of the individual divided by the sum of all the fitnesses
+# use rulette wheel to select the N non_eligible_parents, N = int(population_size*gg)
+# take the inverse of 1-probability
+# use rulette wheel to select the N eligible_parents
+# eligible_partners are the individuals in the population except the non_eligible_individuals
+
+def rulette_wheel(individuals, gg):
+    # calculate the sum of all the fitnesses
+    sum_fitness = 0
+    for individual in individuals:
+        sum_fitness += individual.fitness
+    # calculate the probability of being selected
+    probability = []
+    for individual in individuals:
+        probability.append(individual.fitness / sum_fitness)
+    # use rulette wheel to select the N non_eligible_parents, N = int(population_size*gg)
+    non_eligible_individuals: list[Any] = []
+    for i in range(int(len(individuals) * gg)):
+        # generate a random number between 0 and 1
+        random_num = random.random()
+        # calculate the sum of the probabilities
+        sum_probability = 0
+        for j in range(len(probability)):
+            sum_probability += probability[j]
+            if random_num < sum_probability:
+                non_eligible_individuals.append(individuals[j])
+                break
+
+    # use rulette wheel to select the N eligible_parents
+    eligible_individuals = []
+    for i in range(int(len(individuals) * gg)):
+        # generate a random number between 0 and 1
+        random_num = random.random()
+        # calculate the sum of the probabilities
+        sum_probability = 0
+        for j in range(len(probability)):
+            sum_probability += (1 - probability[j])
+            if random_num < sum_probability:
+                eligible_individuals.append(individuals[j])
+                break
+
+    # eligible_partners are the individuals in the population except the non_eligible_individuals
+    eligible_partners = []
+    for individual in individuals:
+        if individual not in non_eligible_individuals:
+            eligible_partners.append(individual)
+
+    return eligible_individuals, eligible_partners, non_eligible_individuals
