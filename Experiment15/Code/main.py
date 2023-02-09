c5a6180286f5d1245d237777af0e6721a0dd5759
JIAOJIAOMEI <90816748+JIAOJIAOMEI@users.noreply.github.com>
2023-02-09 10:50:57 +0800

    update

--- Experiment15/Code/main.py
+++ Experiment15/Code/main.py
@@ -36,53 +36,6 @@ class AlgorithmParameters:
 
 # create a function to generate parameter combinations using each parameter from above
 # and store it in a dataframe, each row is a combination of parameters
-def generate_parameter_combinations():
-    num_generations = [2000]
-    mutation_rate = [0.2, 0.8]
-    num_individuals = [200]
-    crossover_rate = [0.2, 0.8]
-    mutation_type = ["uniform", "gaussian", "frequency_based"]  # "uniform", "gaussian", "frequency_based"
-    crossover_type = ["one-point", "two-point", "probabilistic", "linear combination",
-                      "average"]  # "two-point", "probabilistic", "linear combination", "average", "Roulette wheel"
-    local_search_rate = [0.2, 0.8]
-    local_search_type = ["uniform", "gaussian"]  # "uniform", "gaussian", "neighbor_search"
-    search_radius = [0.01, 0.1]
-    num_evaluations = [2000]
-    threshold = [0.0001]
-    gg = [0.01, 0.2, 0.5, 0.8, 0.99]
-    dimensions = [50, 400]
-    selection_method = ["SSGA", "sorted_selection_part", "sorted_selection_all",
-                        "roulette_Wheel_Select"]  # "SSGA", "sorted_selection_part", "sorted_selection_all", "roulette_Wheel_Select"
-    fitness_function = [1, 8, 12, 15, 20, 21]
-    algorithm = ["Baseline", "Lamarck", "Baldwin"]  # "Baseline","Lamarck", "Baldwin"
-    parameter_combinations = []
-    for i in num_generations:
-        for j in mutation_rate:
-            for k in num_individuals:
-                for l in crossover_rate:
-                    for m in mutation_type:
-                        for n in crossover_type:
-                            for o in local_search_rate:
-                                for p in local_search_type:
-                                    for q in search_radius:
-                                        for r in num_evaluations:
-                                            for s in threshold:
-                                                for t in gg:
-                                                    for u in dimensions:
-                                                        for v in selection_method:
-                                                            for w in fitness_function:
-                                                                for x in algorithm:
-                                                                    parameter_combinations.append(
-                                                                        [i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x])
-    parameter_combinations = pd.DataFrame(parameter_combinations,
-                                          columns=["num_generations", "mutation_rate", "num_individuals",
-                                                   "crossover_rate", "mutation_type", "crossover_type",
-                                                   "local_search_rate", "local_search_type", "search_radius",
-                                                   "num_evaluations", "threshold", "gg", "dimensions",
-                                                   "selection_method", "fitness_function", "algorithm"])
-    # save the dataframe to a csv file
-    parameter_combinations.to_csv("parameter_combinations.csv", index=True)
-    return parameter_combinations
 
 
 import colorama
@@ -90,7 +43,7 @@ import colorama
 import time
 from datetime import datetime
 
-parameter_combinations = generate_parameter_combinations()
+
 dataframe = pd.read_csv("parameter_combinations.csv", header=0, index_col=0)
 num_rows, num_columns = dataframe.shape
 print("The number of parameter combinations is: " + str(num_rows))
